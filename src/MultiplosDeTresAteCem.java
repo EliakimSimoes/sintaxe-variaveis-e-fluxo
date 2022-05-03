620d658653473785a1dce132ff015bcbd2e5060d
Eliakim Simoes de Matos <eliakim.matos@sensedia.local>
2022-05-03 15:46:36 -0300

    Comitando variaveis e fluxo

---
+++ src/MultiplosDeTresAteCem.java
@@ -0,0 +1,9 @@
+
+class MultiplosDeTresAteCem {
+	
+    public static void main (String[] args) {
+        for (int i = 1; i < 100; i += 3 ){
+                System.out.println(i);
+        }
+    }
+}
